dcf1cea55122426535587a5a78cafe385f364367
José Amilton Tavares Freire Júnior <amilton.freire@icc.org.br>
2026-02-10 10:28:40 -0300

    feat:cria migrations para o banco de dados sqlite

---
+++ Migrations/20260210131911_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace agicommerce_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    Estoque = table.Column<int>(type: "INTEGER", nullable: false),
+                    Categoria = table.Column<string>(type: "TEXT", nullable: false),
+                    ImagemUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    Ativo = table.Column<bool>(type: "INTEGER", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
